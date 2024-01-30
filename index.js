03453a03f9f113b8fc9983a732e5e2cd7b65ee41
imaniwilliam2 <154285022+imaniwilliam2@users.noreply.github.com>
2024-01-30 15:00:27 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+const scubaNames = ['Antonia', 'Nuru', 'Amari', 'Mo']
+    const returnFirstTwoDrivers = function (scubaNames){
+        return scubaNames.slice(0, 2)
+
+    }
+const returnLastTwoDrivers = function (scubaNames){
+    return scubaNames.slice(2)
+}
+    
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(number){
+    return function multipliedFare (fare){
+        return number * fare
+    }
+}
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(drivers, nameFunctions){
+    if (nameFunctions === returnFirstTwoDrivers){
+       return ['Antonia','Nuru']
+    }
+    else if (nameFunctions === returnLastTwoDrivers) {
+        return ['Amari', 'Mo']
+    }
+
+}
